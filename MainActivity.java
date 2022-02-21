b14247448b10c1ff057135d0ea46c2a944b98fd2
gau245 <97627237+gau245@users.noreply.github.com>
2022-02-21 21:00:11 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,96 +1,74 @@
-package com.example.myapplication;
+package com.example.mysaveimage;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.ContentResolver;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.media.ThumbnailUtils;
-import android.net.Uri;
+import android.graphics.Canvas;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.support.v4.app.INotificationSideChannel;
-import android.view.WindowManager;
+import android.view.View;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-import com.google.android.exoplayer2.ui.PlayerView;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import java.util.ArrayList;
+import static android.os.Environment.getExternalStorageDirectory;
 
-public class MainActivity extends AppCompatActivity implements MyAdapter.VideoClickInterface {
+public class MainActivity extends AppCompatActivity {
 
-    RecyclerView idRVvideos;
-    ArrayList<VideoRVModel> list;
-    MyAdapter myAdapter;
-    private  static final int STORAGE_PERMISSION=11;
+    private Bitmap finalEditedImage;
+    private RelativeLayout Relative;
+    ImageView img1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Relative=findViewById(R.id.RelativeLayout);
+        img1=findViewById(R.id.img1);
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createDirectoryAndSaveFile(finalEditedImage);
+                finalEditedImage = getMainFrameBitmap(Relative);
+                Toast.makeText(MainActivity.this, "Successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
 
-        idRVvideos=findViewById( R.id.idRVvideos );
-        list=new ArrayList<>();
-        myAdapter =new MyAdapter(list ,this,this::onVideoclick );
-        idRVvideos.setLayoutManager(new  GridLayoutManager(this,2) );
-        idRVvideos.setAdapter(myAdapter);
+    }
+    private void createDirectoryAndSaveFile(Bitmap imageToSave) {
+        String ts = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()).toString();
+        File direct = new File(getExternalStorageDirectory() + "/ My Qu");
 
-        if (ContextCompat.checkSelfPermission( MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE )== PackageManager.PERMISSION_DENIED){
-            ActivityCompat.requestPermissions( MainActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},STORAGE_PERMISSION );
-        }else {
-            getVideo();
+        if (!direct.exists()) {
+            File wallpaperDirectory = new File("/sdcard/My Qu/");
+            wallpaperDirectory.mkdirs();
+        }
+
+        File file = new File("/sdcard/My Qu/", ts+".png");
+        if (file.exists()) {
+            file.delete();
+        }
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            imageToSave.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
 
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult( requestCode, permissions, grantResults );
-        if (requestCode==STORAGE_PERMISSION){
-                if (grantResults.length>0 &&grantResults[0]==PackageManager.PERMISSION_GRANTED){
-                    Toast.makeText( MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT ).show();
-                    getVideo();
-                }  else {
-                    Toast.makeText( MainActivity.this, "Tha App will not work without permission", Toast.LENGTH_SHORT ).show();
-                    finish();
-                }
-        }
-    }
 
-    private void getVideo() {
-        ContentResolver contentResolver=getContentResolver();
-        Uri uri= MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-        Cursor cursor=contentResolver.query(uri,null,null,null,null);
-        if (cursor!=null && cursor.moveToFirst()){
-            do {
-                @SuppressLint("Range") String videoTitle=cursor.getString( cursor.getColumnIndex( MediaStore.Video.Media.TITLE));
-                @SuppressLint("Range") String videoPath=cursor.getString( cursor.getColumnIndex( MediaStore.Video.Media.DATA));
-                @SuppressLint("Range") Bitmap videoThumbnail= ThumbnailUtils.createVideoThumbnail( videoPath,MediaStore.Images.Thumbnails.MICRO_KIND );
-                list.add(new VideoRVModel( videoTitle,videoPath,videoThumbnail ));
-            }while (cursor.moveToNext());
-        }
-        myAdapter.notifyDataSetChanged();
-    }
-
-    @Override
-    public void onVideoclick(int position) {
-
-        Intent intent=new Intent(MainActivity.this,MainActivity2.class);
-        intent.putExtra( "videoname",list.get( position ).getVideoName());
-        intent.putExtra( "videoPath",list.get( position ).getVideoPath());
-        startActivity(intent);
-
+    private Bitmap getMainFrameBitmap(View view) {
+        Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+        view.draw(new Canvas(bitmap));
+        return bitmap;
     }
 }
